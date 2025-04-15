f4c520fd663c84d7117c1c45a02c45bb0ce141e3
kudnx <zluan009@gmail.com>
2025-04-15 08:31:30 -0300

    modificações

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
 import environ
-from pathlib import Path
 from pathlib import Path
 
 env = environ.Env(
